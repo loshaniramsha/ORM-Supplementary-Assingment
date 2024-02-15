daff00ba11dc633024ddf56edfb610c5930c477e
loshaniramsha <loshanikottagoda@gmail.com>
2024-02-15 20:41:49 +0530

    add

--- src/main/java/orm/example/Main.java
+++ src/main/java/orm/example/Main.java
@@ -15,36 +15,36 @@ public class Main {
         Session session= FactoryConfiguration.getInstance().getSession();
         Transaction transaction= session.beginTransaction();
 
-/*        Author jak=new Author("A1","jak","India",null);
-        Author jeny=new Author("A2","jeny","Australia",null);
-        Author Nowly=new Author("A3","Nowly","England",null);
-        Author josh=new Author("A4","josh","USA",null);
-        Author Mog=new Author("A5","Mog","Japan",null);
+   /*  Author jeny=new Author("A2","jeny","Australia",null);
+     Author Nowly=new Author("A3","Nowly","England",null);
+     Author josh=new Author("A4","josh","USA",null);
+     Author Mog=new Author("A5","Mog","Japan",null);
 
 
-        Book book1=new Book("B1","Java",2022,1000.0,jak);
-        Book book2=new Book("B2","Python",2010,2000.0,jeny);
-        Book book3=new Book("B3","C++",2020,3000.0,jak);
-        Book book4=new Book("B4","C#",2021,4000.0,josh);
-        Book book5=new Book("B5","C",2005,5000.0,Nowly);
+     Book book1=new Book("B1","Java",2022,1000.0,jak);
+     Book book2=new Book("B2","Python",2010,2000.0,jeny);
+     Book book3=new Book("B3","C++",2020,3000.0,jak);
+     Book book4=new Book("B4","C#",2021,4000.0,josh);
+     Book book5=new Book("B5","C",2005,5000.0,Nowly);
 
 
-        List<Book> books=List.of(book1,book3);
-        jak.setBooks(books);
-        jeny.setBooks(List.of(book2));
-        josh.setBooks(List.of(book4));
-        Nowly.setBooks(List.of(book5));
+     List<Book> books=List.of(book1,book3);
+     jak.setBooks(books);
+     jeny.setBooks(List.of(book2));
+     josh.setBooks(List.of(book4));
+     Nowly.setBooks(List.of(book5));
 
-        session.save(jak);
-        session.save(jeny);
-        session.save(josh);
-        session.save(Nowly);
+     session.save(jak);
+     session.save(jeny);
+     session.save(josh);
+     session.save(Nowly);
 
-        session.save(book1);
-        session.save(book2);
-        session.save(book3);
-        session.save(book4);
-        session.save(book5);*/
+     session.save(book1);
+     session.save(book2);
+     session.save(book3);
+     session.save(book4);
+     session.save(book5);*/    Author jak=new Author("A1","jak","India",null);
+
 
        // (01)
 
@@ -59,6 +59,10 @@ public class Main {
         System.out.println("Result: "+result);*/
 
         //(03)
+        Query query= session.createQuery("delete from Book b where b.author.id=:authorId");
+        query.setParameter("authorId","A1");
+        int result= query.executeUpdate();
+        System.out.println("Result: "+result);
 
         //(04)
 
@@ -89,12 +93,12 @@ public class Main {
 
         //(10)
 
-        Query query = session.createQuery("SELECT a.name FROM Author a WHERE ( SELECT COUNT(b.id) FROM Book b WHERE a.id = b.author.id ) > ( SELECT AVG(authorBookCount) FROM ( SELECT COUNT(b.id) AS authorBookCount FROM Book b GROUP BY b.author.id ))");
+/*        Query query = session.createQuery("SELECT a.name FROM Author a WHERE ( SELECT COUNT(b.id) FROM Book b WHERE a.id = b.author.id ) > ( SELECT AVG(authorBookCount) FROM ( SELECT COUNT(b.id) AS authorBookCount FROM Book b GROUP BY b.author.id ))");
         List<String> resultList = query.getResultList();
 
         for (String s : resultList) {
             System.out.println(s);
-        }
+        }*/
 
 
         transaction.commit();
